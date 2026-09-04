519cd3f36d529bacce56b7a1ce2e28b240e73432
Anuchana <shavindaanuchana@gmail.com>
2026-09-04 23:19:15 +0530

    feat: implemnted handle priority sema-down

--- threads/synch.c
+++ threads/synch.c
@@ -118,6 +118,7 @@ sema_up (struct semaphore *sema)
 
   if (!list_empty (&sema->waiters))
   {
+      list_sort(&sema->waiters, cmp_priority, NULL);
       struct thread *t =
           list_entry (list_pop_front (&sema->waiters),
                       struct thread, elem);
